5835ee0b409577ebb42e9ddd0490f0e614cf9d86
nikhilraj286 <nikhilraj286@gmail.com>
2021-06-05 21:15:53 -0700

    setup and login component

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import Login from "./Login";
+import useLocalStorage from "../hooks/useLocalStorage";
+import Dashboard from "./Dashboard";
+
+function App() {
+  const [id, setId] = useLocalStorage("id");
+
+  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+}
+
+export default App;
